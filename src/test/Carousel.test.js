d45b5c54d583aa19871595a1afb3be5748f9bbc1
Arz <zarsalan19@gmail.com>
2022-03-05 18:40:10 -0500

    stateful components tests

--- src/test/Carousel.test.js
+++ src/test/Carousel.test.js
@@ -6,8 +6,26 @@ import CarouselButton from '../CarouselButton';
 
 describe('Carousel', () => {
   let wrapper;
+  const slides = [
+    {
+      imgUrl: 'https://example.com/slide1.png',
+      description: 'Slide 1',
+      attribution: 'Uno Pizzeria',
+    },
+    {
+      imgUrl: 'https://example.com/slide2.png',
+      description: 'Slide 2',
+      attribution: 'Dos Equis',
+    },
+    {
+      imgUrl: 'https://example.com/slide3.png',
+      description: 'Slide 3',
+      attribution: 'Three Amigos',
+    },
+  ];
+
   beforeEach(() => {
-    wrapper = shallow(<Carousel />);
+    wrapper = shallow(<Carousel slides={slides} />);
   });
 
   it('renders a <div>', () => {
@@ -24,5 +42,17 @@ describe('Carousel', () => {
 
   it('renders a CarouselButton labeled `Next`', () => {
     expect(wrapper.find(CarouselButton).at(1).prop('children')).toBe('Next');
+  });
+
+  it('decrements `slideIndex` when Prev is clicked', () => {
+    wrapper.setState({ slideIndex: 1 });
+    wrapper.find('[data-action="prev"]').simulate('click');
+    expect(wrapper.state('slideIndex')).toBe(0);
+  });
+
+  it('increments `slideIndex` when Next is clicked', () => {
+    wrapper.setState({ slideIndex: 1 });
+    wrapper.find('[data-action="next"]').simulate('click');
+    expect(wrapper.state('slideIndex')).toBe(2);
   });
 });
